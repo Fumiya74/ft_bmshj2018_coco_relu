a30b32ca0b4bf4b60ada2beca270a4effbea5e0a
松澤郁哉2 <fumiya@MacBook-Air.local>
2025-09-06 01:05:51 +0900

    データセットクラスの修正

--- src/dataset_coco.py
+++ src/dataset_coco.py
@@ -12,8 +12,10 @@ class ImageFolder224(Dataset):
         self.split = split
         self.use_prepared = use_prepared
         self.input_size = input_size
+
         if use_prepared:
-            self.paths = [p for p in (self.root/split).glob("*") if p.suffix.lower() in IMG_EXTS]
+            # train/ 以下のサブディレクトリも含めて探索
+            self.paths = [p for p in (self.root/split).rglob("*") if p.suffix.lower() in IMG_EXTS]
             self.transform = T.ToTensor()
         else:
             # オンザフライで CenterCrop+Resize
@@ -28,7 +30,8 @@ class ImageFolder224(Dataset):
                 T.ToTensor(),
             ])
 
-    def __len__(self): return len(self.paths)
+    def __len__(self):
+        return len(self.paths)
 
     def __getitem__(self, idx):
         p = self.paths[idx]
